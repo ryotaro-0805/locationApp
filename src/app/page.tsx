1f673c3b523a1074e3ce679ebefcd5d97b09b89d
ryotaro-0805 <rasyouryo@yahoo.co.jp>
2024-09-21 10:45:46 +0900

    修正した。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,30 @@
 'use client'
 import { useEffect, useState } from "react";
 
-interface Weather{
-  name:string;
-  main:{
-    temp:number;
-  };
-  wather:{
-    description:string;
-  }[];
-}
+// interface Weather{
+//   name:string;
+//   main:{
+//     temp:number;
+//   };
+//   wather:{
+//     description:string;
+//   }[];
+// }
 
-interface Location{
-  lat:number|null;
-  lon:number|null;
-}
+// interface Location{
+//   lat:number|null;
+//   lon:number|null;
+// }
 
 export default function Home():JSX.Element {
   // const [weather, setWeather]=useState<Weather|null>(null);
-  const [location,setLocation]=useState<Location>({lat:null,lon:null});
+  // const [location,setLocation]=useState<Location>({lat:null,lon:null});
   const [address,setAddress]=useState<string|null>(null);
   useEffect(()=>{
     if(navigator.geolocation){
       navigator.geolocation.getCurrentPosition((position)=>{
         const {latitude,longitude}=position.coords;
-        setLocation({lat:latitude,lon:longitude});
+        // setLocation({lat:latitude,lon:longitude});
         fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyDzTvZtowEb7v1SRNizfQ-D3vNZOzi7HDQ`)
   .then(response => response.json())
   .then((data)=>{
