cfac62a0db29044095e2cb8b24ba426c3e0c539c
ryotaro-0805 <rasyouryo@yahoo.co.jp>
2024-09-21 11:07:10 +0900

    修正した。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home():JSX.Element {
     if(navigator.geolocation){
       navigator.geolocation.getCurrentPosition((position)=>{
         const {latitude,longitude}=position.coords;
-        fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyDzTvZtowEb7v1SRNizfQ-D3vNZOzi7HDQ`)
+        fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}`)
   .then(response => response.json())
   .then((data)=>{
     const formattedAddress=data.results[0]?.formatted_address;
